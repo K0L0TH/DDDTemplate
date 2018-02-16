f694ef99f3f604f7fe205d2c9261d7b20a3156e9
Yousria <yousria.benchadi@gmail.com>
2018-02-16 14:45:10 +0100

    Repository

---
+++ src/main/java/com/app/ddd/core/repository/Repository.java
@@ -0,0 +1,86 @@
+package com.app.ddd.core.repository;
+
+import com.app.ddd.core.contractualisation.Contrat;
+import com.app.ddd.core.sourcing.CTO;
+import com.app.ddd.core.sourcing.Candidat;
+import com.app.ddd.core.sourcing.ChargeRecrutement;
+import com.app.ddd.core.sourcing.ConsultantRecruteur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bench on 16/02/2018.
+ */
+public class Repository {
+
+    private List<Candidat> candidateList;
+    private List<ChargeRecrutement> cRList;
+    private List<ConsultantRecruteur> recruteurList;
+    private List<Contrat> contrats;
+    private CTO cto;
+
+    public Repository(CTO cto){
+        this.candidateList = new ArrayList<Candidat>();
+        this.cRList = new ArrayList<ChargeRecrutement>();
+        this.recruteurList = new ArrayList<ConsultantRecruteur>();
+        this.contrats = new ArrayList<Contrat>();
+        this.cto = cto;
+    }
+
+    public void addContrat(Contrat contrat){
+        this.contrats.add(contrat);
+    }
+
+    public void addCandidat(Candidat candidat){
+        this.candidateList.add(candidat);
+    }
+
+    public void addCR(ChargeRecrutement CR){
+        this.cRList.add(CR);
+    }
+
+    public void addRecruteur(ConsultantRecruteur recruteur){
+        this.recruteurList.add(recruteur);
+    }
+
+    public List<Candidat> getCandidateList() {
+        return candidateList;
+    }
+
+    public void setCandidateList(List<Candidat> candidateList) {
+        this.candidateList = candidateList;
+    }
+
+    public List<ChargeRecrutement> getcRList() {
+        return cRList;
+    }
+
+    public void setcRList(List<ChargeRecrutement> cRList) {
+        this.cRList = cRList;
+    }
+
+    public List<ConsultantRecruteur> getRecruteurList() {
+        return recruteurList;
+    }
+
+    public void setRecruteurList(List<ConsultantRecruteur> recruteurList) {
+        this.recruteurList = recruteurList;
+    }
+
+    public CTO getCto() {
+        return cto;
+    }
+
+    public void setCto(CTO cto) {
+        this.cto = cto;
+    }
+
+    public List<Contrat> getContrats() {
+        return contrats;
+    }
+
+    public void setContrats(List<Contrat> contrats) {
+        this.contrats = contrats;
+    }
+}
