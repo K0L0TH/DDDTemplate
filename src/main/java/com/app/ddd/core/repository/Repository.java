a7cc5e02d546e38312c8ef0d20b5c0a6feaec305
Yousria <yousria.benchadi@gmail.com>
2018-02-16 14:55:47 +0100

    fix repository

--- src/main/java/com/app/ddd/core/repository/Repository.java
+++ src/main/java/com/app/ddd/core/repository/Repository.java
@@ -1,6 +1,7 @@
 package com.app.ddd.core.repository;
 
 import com.app.ddd.core.contractualisation.Contrat;
+import com.app.ddd.core.contractualisation.ContratAggregate;
 import com.app.ddd.core.sourcing.CTO;
 import com.app.ddd.core.sourcing.Candidat;
 import com.app.ddd.core.sourcing.ChargeRecrutement;
@@ -17,18 +18,18 @@ public class Repository {
     private List<Candidat> candidateList;
     private List<ChargeRecrutement> cRList;
     private List<ConsultantRecruteur> recruteurList;
-    private List<Contrat> contrats;
+    private List<ContratAggregate> contrats;
     private CTO cto;
 
     public Repository(CTO cto){
         this.candidateList = new ArrayList<Candidat>();
         this.cRList = new ArrayList<ChargeRecrutement>();
         this.recruteurList = new ArrayList<ConsultantRecruteur>();
-        this.contrats = new ArrayList<Contrat>();
+        this.contrats = new ArrayList<ContratAggregate>();
         this.cto = cto;
     }
 
-    public void addContrat(Contrat contrat){
+    public void addContrat(ContratAggregate contrat){
         this.contrats.add(contrat);
     }
 
@@ -76,11 +77,11 @@ public class Repository {
         this.cto = cto;
     }
 
-    public List<Contrat> getContrats() {
+    public List<ContratAggregate> getContrats() {
         return contrats;
     }
 
-    public void setContrats(List<Contrat> contrats) {
+    public void setContrats(List<ContratAggregate> contrats) {
         this.contrats = contrats;
     }
 }
